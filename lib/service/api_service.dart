7e1bf6ba93650fb573ea497c8f9653805d3b7f74
belal_soliman <belalsoliman8@gmail.com>
2024-12-15 03:12:35 +0200

    Add ApiService class for fetching popular movies and update dependencies

---
+++ lib/service/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:http/http.dart' as http;
+import 'package:movies/constants/api_const.dart';
+
+class ApiService {
+  Future<void> fetchMovie({int page = 1}) async {
+    final url =
+        Uri.parse("${ApiConst.baseUrl}/movie/popular?language=en-US&page=1");
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      log(data);
+    } else {
+      log(404);
+    }
+  }
+}
