0df85cc3c0c7e07d9baf9cc36a1188fff06592da
belal_soliman <belalsoliman8@gmail.com>
2024-12-28 10:57:28 +0200

    Implement theme management using Bloc; add ThemeBloc, events, and states; refactor MyApp to use MultiBlocProvider and BlocBuilder for dynamic theme switching.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/view_models/theme/theme_bloc.dart';
 
 import 'constants/my_theme_data.dart';
 import 'screens/movies_screen.dart';
@@ -23,16 +25,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: getIt<NavigationService>().navigatorKey,
-      debugShowCheckedModeBanner: false,
-      title: 'Movies App',
-      theme: MyThemeData.lightTheme,
-      home: const MoviesScreen(),
-      // const SplashScreen(), //const MovieDetailsScreen(), //const FavoritesScreen(), //const MoviesScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ThemeBloc(),
+        ),
+        // Add other BlocProviders here if needed
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            navigatorKey: getIt<NavigationService>().navigatorKey,
+            debugShowCheckedModeBanner: false,
+            title: 'Movies App',
+            theme: state is LightThemeState
+                ? MyThemeData.lightTheme
+                : MyThemeData.darkTheme,
+            home: const MoviesScreen(),
+            // const SplashScreen(), //const MovieDetailsScreen(), //const FavoritesScreen(), //const MoviesScreen(),
+          );
+        },
+      ),
     );
   }
 }
